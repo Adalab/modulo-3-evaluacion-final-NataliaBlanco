9d58a1f307261ba20bd6a1ae3b8bce2bc9363b2b
Natalia <nataliaouteiral@gmail.com>
2023-05-16 20:41:28 +0200

    fix some styling

--- src/components/characters/CharacterDetail.js
+++ src/components/characters/CharacterDetail.js
@@ -5,34 +5,44 @@ import { faHeartbeat } from '@fortawesome/free-solid-svg-icons';
 import { faSkull } from '@fortawesome/free-solid-svg-icons';
 const CharacterDetail = ({ characterFind }) => {
   return characterFind ? (
-    <div className="detail">
-      <Link to="/">
-        <button className="detail_button">Back</button>
-      </Link>
-      <section className="detail_card">
-        <img
-          className="detail_card_img"
-          src={characterFind.image === '' ? img : characterFind.image}
-          alt="castle"
-        ></img>
-        <p className="detail_card_n">{characterFind.name}</p>
-        <p className="detail_card_a">
-          Status:
-          {characterFind.alive === true ? (
-            <>
-              alive <FontAwesomeIcon icon={faHeartbeat} />
-            </>
-          ) : (
-            <>
-              dead <FontAwesomeIcon icon={faSkull} />
-            </>
-          )}
-          {'   '}
-        </p>
-        <p className="detail_card_g">Gender: {characterFind.gender}</p>
-        <p className="detail_card_s">Specie: {characterFind.species}</p>
-        <p className="detail_card_h">House: {characterFind.house}</p>
-      </section>
+    <div className="body-detail">
+      <div className="detail">
+        <Link to="/">
+          <button className="detail_button">Back</button>
+        </Link>
+        <section className="detail_card">
+          <img
+            className="detail_card_img"
+            src={characterFind.image === '' ? img : characterFind.image}
+            alt="castle"
+          ></img>
+          <section className="detail_card_section">
+            <p className="detail_card_section_n">{characterFind.name}</p>
+            <p className="detail_card_section_a">
+              Status:
+              {characterFind.alive === true ? (
+                <>
+                  alive <FontAwesomeIcon icon={faHeartbeat} />
+                </>
+              ) : (
+                <>
+                  dead <FontAwesomeIcon icon={faSkull} />
+                </>
+              )}
+              {'   '}
+            </p>
+            <p className="detail_card_section_p">
+              Gender: {characterFind.gender}
+            </p>
+            <p className="detail_card_section_p">
+              Specie: {characterFind.species}
+            </p>
+            <p className="detail_card_section_p">
+              House: {characterFind.house}
+            </p>
+          </section>
+        </section>
+      </div>
     </div>
   ) : (
     ' No se ha encontrado la página '
